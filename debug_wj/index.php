6481497382f4ab1aac8d27ba61cdeb5e8a417489
tq5124 <wutianqi124@gmail.com>
2013-10-22 14:49:24 +0800

    debug for wj

---
+++ debug_wj/index.php
@@ -0,0 +1,88 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wjddjb!</title>
+
+    <link rel="stylesheet" href="http://tq5124.dongyueweb.com/lib/bootstrap3/css/bootstrap.min.css">
+<script src="http://tq5124.dongyueweb.com/lib/jquery/jquery-1.10.2.min.js"></script>
+<script src="http://tq5124.dongyueweb.com/lib/bootstrap3/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <header class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar">a</span>
+            <span class="icon-bar">b</span>
+            <span class="icon-bar">c</span>
+          </button>
+          <a class="navbar-brand" href="http://v3.bootcss.com/examples/offcanvas/#">项目名</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://v3.bootcss.com/examples/offcanvas/#">Home</a></li>
+            <li><a href="http://v3.bootcss.com/examples/offcanvas/#about">About</a></li>
+            <li><a href="http://v3.bootcss.com/examples/offcanvas/#contact">Contact</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </header><!-- /.navbar -->
+
+    <div class="container main">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 main_side">
+                <div class="list-group">
+                    <a href="http://v3.bootcss.com/examples/offcanvas/#" class="list-group-item active">
+                        <span class="badge">14</span>Link
+                    </a>
+                    <a href="http://v3.bootcss.com/examples/offcanvas/#" class="list-group-item">
+                        <span class="badge">1</span>aaa
+                    </a>
+                    <a href="http://v3.bootcss.com/examples/offcanvas/#" class="list-group-item">
+                        <span class="badge">0</span>bbb
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 main_side">
+                <ul class="media-list">
+                    <li class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACnElEQVR4Xu2Y64upURTGFzLJPVGuo3wg95IyU+JvZ3KJUiKhlOKLEUPuxuWctcrUOc6UebeTZO1PXt699lrPfvb6ZcvG4/ERHnjIWAB2AB8B7gEP3AOBmyBTgCnAFGAKMAUeWAHGIGOQMcgYZAw+MAT4zxBjkDHIGGQMMgYZg1dQYL/fQ6lUgnA4DFqt9izi72YL6/Ua7Hb7RatdO953i14Ng41GAwaDAaTTaZDL5X+sh8W8vb2Bz+cDm812kQDXjvdfBcDdrVQqkEgkQKfTna1Vq9VInGAwSALgZ5VKBQaDgd59f3+Hp6cnMBqN9PzTeBcp+s1Lwg447a7FYqEC/x5YLAqAx+L5+ZmOQK/Xg3a7DalUCpbLJZTLZYjH4ySAlHg3FaDZbEK/3//KIRQKgdVqpeftdkvWDwQCMJ/PQa1Wg8PhoN/QMavVit5xu93g8Xjoe6nxpIog5IDdbgfZbBa8Xi84nU4SotVq0c4qlUooFotk7VgsBnim0fInAXCncS4ehZeXF5DJZCAS7yYCjEYjsjc2PizgeDxSUeiCj48P6Ha7Z3md+sBwOIRqtUq/v76+gkajAZF4NxEALZzL5SCZTNJO4nM+n6cdRTEOhwPlhVTodDr0DlodxUKh/H4/TKdTaoIYY7PZSIqHbpM6hI4ALnrq8GhvLAabYTQaPcunXq/TEXC5XFAoFOiIYONDoTKZDJhMJohEIpLiSS0e5wkLgEHQzrPZDPR6PZjN5n/mg+5QKBTUEyaTCYmBTsDx+fkJi8XiC4M/jXdzAUQSuPXcqzjg1kWIrM8C8I0Q3wjxjRDfCIl00XufyxRgCjAFmAJMgXvv5CL5MwWYAkwBpgBTQKSL3vtcpgBTgCnAFGAK3HsnF8mfKfDoFPgFoIpOn0g4LMEAAAAASUVORK5CYII=" alt="...">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            ...
+                        </div>
+                    </li>
+                    <li class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACnElEQVR4Xu2Y64upURTGFzLJPVGuo3wg95IyU+JvZ3KJUiKhlOKLEUPuxuWctcrUOc6UebeTZO1PXt699lrPfvb6ZcvG4/ERHnjIWAB2AB8B7gEP3AOBmyBTgCnAFGAKMAUeWAHGIGOQMcgYZAw+MAT4zxBjkDHIGGQMMgYZg1dQYL/fQ6lUgnA4DFqt9izi72YL6/Ua7Hb7RatdO953i14Ng41GAwaDAaTTaZDL5X+sh8W8vb2Bz+cDm812kQDXjvdfBcDdrVQqkEgkQKfTna1Vq9VInGAwSALgZ5VKBQaDgd59f3+Hp6cnMBqN9PzTeBcp+s1Lwg447a7FYqEC/x5YLAqAx+L5+ZmOQK/Xg3a7DalUCpbLJZTLZYjH4ySAlHg3FaDZbEK/3//KIRQKgdVqpeftdkvWDwQCMJ/PQa1Wg8PhoN/QMavVit5xu93g8Xjoe6nxpIog5IDdbgfZbBa8Xi84nU4SotVq0c4qlUooFotk7VgsBnim0fInAXCncS4ehZeXF5DJZCAS7yYCjEYjsjc2PizgeDxSUeiCj48P6Ha7Z3md+sBwOIRqtUq/v76+gkajAZF4NxEALZzL5SCZTNJO4nM+n6cdRTEOhwPlhVTodDr0DlodxUKh/H4/TKdTaoIYY7PZSIqHbpM6hI4ALnrq8GhvLAabYTQaPcunXq/TEXC5XFAoFOiIYONDoTKZDJhMJohEIpLiSS0e5wkLgEHQzrPZDPR6PZjN5n/mg+5QKBTUEyaTCYmBTsDx+fkJi8XiC4M/jXdzAUQSuPXcqzjg1kWIrM8C8I0Q3wjxjRDfCIl00XufyxRgCjAFmAJMgXvv5CL5MwWYAkwBpgBTQKSL3vtcpgBTgCnAFGAK3HsnF8mfKfDoFPgFoIpOn0g4LMEAAAAASUVORK5CYII=" alt="...">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            ...
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6 main_side">
+                <textarea id="main_textArea" class="form-control"></textarea>
+            </div>
+        </div>
+    </div>
+</body>
+
+<style>
+.main_side{
+    border-right: 2px solid #ccc;
+}
+</style>
+<script>
+    var h = $(window).height() * 0.7;
+    $('#main_textArea').height(h);
+</script>
+
+</html>
